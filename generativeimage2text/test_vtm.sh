42cc0f804ce0d4a6d85853851e9117b316b617f9
dcb <dongchengbo@ruc.edu.cn>
2023-05-08 16:24:43 +0800

    del sth

--- generativeimage2text/test_vtm.sh
+++
@@ -1,9 +0,0 @@
-TOKENIZERS_PARALLELISM=false CUDA_VISIBLE_DEVICES=$1 python infer_like_mplug.py \
-    --config ../config/train_vtmsparse_bv.yaml \
-    --output_dir ../ckpt/results/ \
-    --checkpoint ../ChinaOpenCkpt/GVT_ft_ChinaOpen.pth \
-    --min_length 15 \
-    --beam_size 10 \
-    --max_length 32 \
-    --max_input_length 48 \
-    --vtm_file ../data/cmo_total_cntags_0422.txt
