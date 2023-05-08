84e63081dbf296da8f3e0fd59afa1518793bc448
dcb <dongchengbo@ruc.edu.cn>
2023-05-08 16:23:02 +0800

    del sth

--- down.sh
+++
@@ -1,2 +0,0 @@
-AZFUSE_TSV_USE_FUSE=1 python -m generativeimage2text.inference -p \
-"{'type': 'download_model'}"
--- train.sh
+++
@@ -1,4 +0,0 @@
-CUDA_VISIBLE_DEVICES=$1 python -m generativeimage2text.train_dcb -p "{'type': 'dcb_train', \
-'yaml': '/home/dcb/code/bv/git_aimc/config/train_vatex.yaml', \
-'description': 'freezeall', \
-            }"
